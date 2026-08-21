40acbc8eb4557aa239ab402c16673713239972d8
4gymqusais <backupgym22@gmail.com>
2026-08-21 18:42:16 +0400

    Update LoginScreen.jsx

--- attendance-tablet-app/src/screens/LoginScreen.jsx
+++ attendance-tablet-app/src/screens/LoginScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './styles/LoginScreen.css';
+import './LoginScreen.css';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
