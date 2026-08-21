119be9a019ae4a84d0a311d69f60936be8fcb076
4gymqusais <backupgym22@gmail.com>
2026-08-21 18:42:38 +0400

    Update AttendanceScreen.jsx

--- attendance-tablet-app/src/screens/AttendanceScreen.jsx
+++ attendance-tablet-app/src/screens/AttendanceScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
-import './styles/AttendanceScreen.css';
+import './AttendanceScreen.css';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
